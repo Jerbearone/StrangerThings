e29f5e7f97da513f850500203579dc16daac09b4
= <jwparker284@gmail.com>
2023-08-17 09:15:31 -0700

    displays all posts in cohort

--- src/components/PostsCard.jsx
+++ src/components/PostsCard.jsx
@@ -11,16 +11,45 @@ const URL = `${BASEURL}/${COHORT}`;
 const GETPOSTS = `${URL}/posts`;
 
 
-function sendTempData() {
 
+function SingleCard({post}) {
+ 
+  //Single card that will be mapped
+  console.log("posts");
+  console.log(post);
+  return (
+    <Card className='post_card'>
+    <Card.Header>{post.author.username}</Card.Header>
+    <Card.Body>
+      <blockquote className="blockquote mb-0">
+      <p>
+          {' '}
+          {post.title}
+          {' '}
+        </p>
+        <p>
+          {' '}
+          {post.description}
+          {' '}
+        </p>
+        <footer className="blockquote-footer">
+          Price: <cite title="Source Title">{post.price}</cite>
+        </footer>
+      </blockquote>
+    </Card.Body>
+  </Card>
+
+  )
 }
 
 function PostsCard() {
+  const [posts, setPosts] = useState([])
   useEffect(()=> {
     const getPosts = async () => {
       try{
         const response = await fetch(GETPOSTS);
         const data = await response.json();
+        setPosts(data.data.posts);
         console.log(data);
       }catch(error){
         console.log(error)
@@ -29,27 +58,17 @@ function PostsCard() {
     }
 
     getPosts();
-  
-  }, [])
-
-
+  }, []);
 
   return (
-    <Card>
-      <Card.Header>Quote</Card.Header>
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p>
-            {' '}
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-            posuere erat a ante.{' '}
-          </p>
-          <footer className="blockquote-footer">
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
+    <div>
+      {
+        posts.map((post) => {
+          //return <h1 key={post.author._id}>{post.author.username}</h1>
+          return(<SingleCard key={post.author._id} post={post}></SingleCard>)
+        })
+      }
+    </div>
   );
 }
 
