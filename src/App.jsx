1e8d0e6ff78e06e619db671415c3c431f7c0f6e3
= <jwparker284@gmail.com>
2023-08-17 04:50:55 -0700

    start of project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css';
+import NavbarComponent from './components/NavbarComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <NavbarComponent></NavbarComponent>
+
+    </>
+  )
+}
+
+export default App
