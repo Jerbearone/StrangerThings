1895406a3e554657b438d7277bf058b9c0b8ff8f
= <jwparker284@gmail.com>
2023-08-21 20:30:30 -0700

    updated home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react"
+import { getUserData } from "../networking/api/api";
+import { getToken } from "../networking/localStorage/localStorage";
+
 export default function Home() {
+    const [username, setUsername] = useState(null);
+    const token = getToken();
+    //create fetch call to get username;
+    useEffect(()=> {
+        
+        const getData = async()=> {
+            const data= await getUserData(token);
+            setUsername(data.data.username);
+            console.log(data.data.username);
+            console.log(data)
+
+        }
+        getData();
+        
+    },[])
+
     return (
         <div className="home_page">
-            <h3>Welcome to Stranger Things!{}</h3>
+            <h3>Welcome to Stranger Things {username !== null && username}!</h3>
 
         </div>
     )
